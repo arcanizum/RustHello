dca98b31273bce18638bc6e7514bcc04d74274a1
Avently <avently@local>
2023-01-31 16:43:15 +0300

    Prints

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,6 @@ fn main() {
     println!("Hello, world!");
     println!("Glad to see you here.");
     println!("Let's start experimenting with RUST!");
+
+    println!("First of all, let's do some prints...");
 }
